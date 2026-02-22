a5eaf571c13a4dd9acbef1dad5aa5b1cdf2f5d7e
Tji Fung <masterlanyard@gmail.comvercel>
2026-02-22 18:57:33 +0700

    Remove vercel.json - let Vercel auto-detect

--- dist/dev/server/chunks/ssr/_next-internal_server_app_ai-chat_page_actions_5afde153.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/ai-chat/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_ai-chat_page_actions_5afde153.js.map
--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"x6E/bNPLYYt12VxDcSrBGbiriLEI8EPmGLKyJXKkeE0=\"\n}"
